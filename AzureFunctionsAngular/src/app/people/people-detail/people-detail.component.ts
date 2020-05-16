7362ff2af322efafc2a1ea2921069256cfe4e583
David Sheardown <davidsheardown@hotmail.com>
2020-05-16 18:51:23 +0100

    first step for person detail with routing

---
+++ AzureFunctionsAngular/src/app/people/people-detail/people-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-people-detail',
+  templateUrl: './people-detail.component.html',
+  styleUrls: ['./people-detail.component.css']
+})
+export class PeopleDetailComponent implements OnInit {
+
+  constructor() { }
+
+  PersonId: string = null;
+
+  ngOnInit(): void {
+  }
+
+}
--- AzureFunctionsAngular/src/app/people/people.module.ts
+++ AzureFunctionsAngular/src/app/people/people.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { PeopleComponent } from './people.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { PeopleDetailComponent } from './people-detail/people-detail.component';
 
 
 @NgModule({
   declarations: [
-    PeopleComponent
+    PeopleComponent,
+    PeopleDetailComponent
   ],
   imports: [
     SharedModule,
     RouterModule.forChild([
-      {path: 'people', component: PeopleComponent}
+      {path: 'people', component: PeopleComponent},
+      {path: 'people/:id', component: PeopleDetailComponent}
     ])
   ]
 })
